8e3e1ee9da30785bf96542b3b50bd076d73a8815
tabcode <tabcodet@gmail.com>
2021-03-14 18:18:12 -0600

    ok

--- App.js
+++ App.js
@@ -1,21 +1,87 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity, Alert } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import * as Sharing from 'expo-sharing';
 
-export default function App() {
+const App = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  let openImagePickerAsync = async () => {
+    const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (permissionResult.granted === false) {
+      alert('Perission to access camera is required.');
+      return;
+    }
+    const pickerResult = await ImagePicker.launchImageLibraryAsync();
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+    setSelectedImage({ localUri: pickerResult.uri });
+  }
+  const openShareDialog = async () => {
+    const result = await Sharing.isAvailableAsync();
+    if (!result) {
+      alert("Sharing is not available in your platform.");
+      return;
+    }
+    await Sharing.shareAsync(selectedImage.localUri);
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+      <Text style={styles.title}>Pick an Image!</Text>
+      <TouchableOpacity
+        onPress={openImagePickerAsync}
+      >
+        <Image
+          source={{ uri: selectedImage !== null ? selectedImage.localUri : "https://static.scientificamerican.com/sciam/cache/file/D78728AD-1FD6-431E-9F2933C6D544D339_source.jpg" }}
+          style={styles.img}
+        />
+      </TouchableOpacity>
 
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => Alert.alert('Hello!!')}
+      >
+        <Text style={styles.buttonText}>Press me</Text>
+      </TouchableOpacity>
+
+      {
+        selectedImage ?
+          <TouchableOpacity
+            style={styles.button}
+            onPress={openShareDialog}
+          >
+            <Text style={styles.buttonText}>Share this image</Text>
+          </TouchableOpacity>
+          : <View />
+      }
+    </View>
+  )
+};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#292929'
   },
+  title: {
+    fontSize: 30,
+    color: '#fff'
+  },
+  img: {
+    height: 200,
+    width: 200,
+    resizeMode: 'contain'
+  },
+  button: {
+    backgroundColor: 'blue',
+    marginTop: 10,
+    padding: 10
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 20
+  }
 });
+export default App;
